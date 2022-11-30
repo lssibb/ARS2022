96f2a1a0fca7e2bb553b293891e41ec23f1fc075
ARS <114455833+lssibb@users.noreply.github.com>
2022-11-30 09:49:55 +0700

    Update QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -1,13 +1,21 @@
 a=['Sin30?','tg45','tg*ctg','1 Алкан','2+2']
 b=['0.5','1','1','метан','4']
-count=0
-for i in range (len(a)):
-    print(a[i])
+cond=1
+def quiz():
+    count=0
+    for i in range (len(a)):
+        print(a[i])
+        ans=input()
+        if ans==b[i]:
+            print('Правильно')
+            count+=1
+        else:
+            print('Неправильно')
+    print("Result")
+    print(count)
+while cond==1:
+    quiz()
+    print("Закончить?")
     ans=input()
-    if ans==b[i]:
-        print('Правильно')
-        count+=1
-    else:
-        print('Неправильно')
-print("Количество правильных ответов")
-print(count)
+    if ans =="Да":
+        cond=0

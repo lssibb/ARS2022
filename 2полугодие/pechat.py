33ad7886443d14e87eedb1a6c53874d6aba929f1
ARS <114455833+lssibb@users.noreply.github.com>
2023-04-11 10:09:17 +0700

    Add files via upload

---
+++ 2полугодие/pechat.py
@@ -0,0 +1,11 @@
+s=list(input())
+m=["** **","* * *","*   *"]
+i=["*   *","*  **","* * *","**  *"]
+##for i in range(len(s)):
+    ## 1 str
+for i in range  (len(s)) :
+    if sprint(f"{m[0]}",end="$")
+print(f"{i[0]}")
+print(f"{m[1]} {i[1]}")
+print(f"{m[2]} {i[2]}")
+print(f"{m[2]} {i[3]}")
